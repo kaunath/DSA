3b5b09bf3e35871af75d364f87aaf48bc397b8d0
Kaushik <nathkaushik1996@gmail.com>
2021-07-13 19:26:17 +0530

    testing

--- STL/1_array.cpp
+++ STL/1_array.cpp
@@ -3,33 +3,10 @@
 using namespace std; 
 
 /* 	
-
- refer to the notes of array in docs
-max size of array in c++ if defined as global
-	
-	1. bool arr[100000000]; i.e 10^8 for boolean
-	2. int arr [10000000];  i.e 10^7 for int, double, char
-
-
-	array<int, 3> arr; // -> {0, 0, 0} cause by default value 
-	is 0 for globally defined array
+ 	refer to the notes in array docs for theory
 */
 
 int main() {
-
-	/* 
-	max size of 10^6 -> int, double, char 
-	int arr[1000000]; 
-
-	max size of 10^7 -> bool
-	bool arr[10000000]; 
-
-	traditional way to define an array -> int arr[100]; 
-
-	array<int, 3> arr;  {?, ?, ?} i.e garbage value for locally defined array
-	array<int, 5> arr = {1}; if we define at least one value, remaining spaces will be 0 {1, 0, 0, 0, 0}
-
-*/
 
 	array<int, 5> arr; 
 	
@@ -45,27 +22,6 @@ int main() {
 	// 2. arr.at(index)
 	cout << arr.at(2); //print element at index 2
 
-	/*
-	iterators -> 
-	1.begin() --> points to first
-	2.end()  --> points to the next of the last
-	3.rbegin() --> points to the last, reverse begin, 
-	4.rend() --> points to the before of first 
-
-*/
-	
-	// for(auto it: arr.begin(); it!=arr.end(); it++){
-	// 	cout << *it << " ";
-	// }
-
-	// for(auto it:  arr.rbegin(); it>arr.rend();it++) {
-	// 	cout << *it << " "; 
-	// }
-
-	// for(auto it: arr.end() - 1; it>=arr.begin();it--) {
-	// 	cout << *it << " "; 
-	// }
-
 	// for each loop 
 	for(auto it: arr) {
 		cout << it << " "; 
@@ -76,4 +32,15 @@ int main() {
 	for(auto c:s) {
 		cout << c << " "; 
 	}
+
+	/* size 
+	cout << arr.size();  //prints size
+
+	// front 
+	cout << arr.front(); // or arr.at(0); for begin element 
+
+	// back
+	cout << arr.back(); // or arr.at(arr.size() - 1); for last element
+	*/
+
 }
