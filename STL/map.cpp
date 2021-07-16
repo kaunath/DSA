2b0d54fa83099753fb9d841d88e7922f7ebbdc4e
Kaushik <nathkaushik1996@gmail.com>
2021-07-16 19:46:10 +0530

    add file for set map

---
+++ STL/map.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include<iterator> 
+using namespace std; 
+
+
+/*
+refer to the notes for vector docs in theory
+*/
+int main()
+{
+	// Key Value 
+	// raj -> 27 
+	// hima -> 31 
+	// sandeep -> 67 
+	// tank -> 89 
+	// map only stores unique keys 
+	// log n is the tc of map 
+
+	map<string, int> mpp; 
+	mpp["raj"] = 27; 
+	mpp["hima"] = 31; 
+	mpp["tank"] = 89; 
+	mpp["raj"] = 29; 
+	mpp.emplace("raj", 45); 
+	mpp.erase("raj"); // mpp.erase(key) 
+	//mpp.erase(mpp.begin()); // mpp.erase(iterator)
+	//mpp.clear(); // entire map is cleaned up
+	//mpp.erase(mpp.begin(), mpp.begin()+2); // cleans up a given range
+	auto it = mpp.find("raj"); // points to where raj lies 
+	auto it_2 = mpp.find("simran"); // points to end since she does not exists 
+
+	if(mpp.empty()) {
+		cout << "Yes it is empty"; 
+	}
+	mpp.count("raj"); // always returns 1 as it stores only 1 instance of raj 
+	
+	/*
+	pair<int,int> pr;
+	pr.first = 1; 
+	pr.second = 10; 
+	*/
+	// printing map same as pair
+	for(auto it: mpp) {
+		cout << it.first << " " << it.second << endl; 
+	}
+	//or
+	cout << "another way" << endl;
+	for(auto it = mpp.begin(); it!=mpp.end();it++) {
+		cout << it->first << " " << it->second << endl; 
+	}
+
+	/* Won't work
+	for(auto it = mpp.begin(); it != mpp.end(); it++) {
+		cout << *it.first << " " << *it.second << endl; 
+	}
+	*/
+
+	// does not stores in any order 
+	unordered_map<int,int> un_mpp; 
+	/* 
+		unordered_map<pair<int,int>,int> mpp; it is possible
+	 	o(1) in almost all the operations
+		 o(n) in the worst case, where n is the container size 
+	 */
+
+	// Pair class 
+	pair<int,int> pr = {1,2}; 
+	pair< pair<int,int>, int> pr_2 = {{1,2}, 2}; 
+	cout << pr_2.first.second << endl; // 2
+	pair<pair<int,int>, pair<int,int>> pr_3 = {{1,2},{2, 4}};
+	cout << pr_3.first.first; // 1 
+	cout << pr_3.second.second; // 4 
+
+	/*
+	Also possible
+	vector<pair<int,int>> vec; 
+	set<pair<int,int>> st; 
+	map< pair<int,int>, int> mpp; 
+	*/
+
+	multimap<string, int> muul_mpp;
+	mpp.emplace("raj", 2); 
+	mpp.emplace("raj", 5); 
+
+}
--- STL/set.cpp
+++ STL/set.cpp
@@ -17,27 +17,89 @@ int main(int argc, char const *argv[])
 
 	cout << "Set size = "<<st.size()<< endl;
 
-	// erase functionality, takes logn time
-	// st.erase(st.begin()); // st.erase(iterator) // st -> {2, 5}
-
-	// st.erase(st.begin(), st.begin()+ 2); // -> [)
-	// // st.erase(startIterator, endIterator) 
-
-	st.erase(5); // st.erase(key) // delete the 5 -> {1, 2}
-for (auto it: st)
-{
-	cout << it << " ";
-	/* code */
-}
-cout << '\n';
-
-cout << "Set size = "<<st.size()<< endl;
-
-	// log n 
+	/* erase functionality, takes logn time
+	3 types of erase functionality
 	
+	1.st.erase(st.begin()); // st.erase(iterator) 
+	2. st.erase(st.begin(), st.begin()+ 2); // st.erase(start_iterator, end_iteraor) 
+	3.st.erase(5); // st.erase(key) 
+	*/
+	st.erase(5); // st.erase(key) 
+	for (auto it: st)
+	{
+		cout << it << " ";
+		
+	}
+	/*
+	or 
+	for(auto it=st.begin(); it!=st.end();it++) {
+		cout << *it << " "; 
+	}
+	*/
+	cout << '\n';
 
+	cout << "Set size = "<<st.size()<< endl;
 
+	
+	/*
+	find() method returns an iterator pointing to the element,
+	if the element is not found, then it points to st.end
+	*/
+	set<int> st_2 = {1, 5, 7, 8}; 
 
+	auto it_2 = st_2.find(7); // log n // it will be iterator to 7
+	cout << "find element is " << *it_2 <<  endl;
+	auto it_3 = st_2.find(9); // it = st.end(); 
+	cout << "find element is " << *it_3 <<  endl;
+	st_2.emplace(6); // st.insert(6) 
+
+	cout << "Set size is "<<st_2.size() << endl; 
+
+	unordered_set<int> un_st; 
+	/*
+	 average time complexity is O(1) 
+	if tle -> switch to set 
+	 but the worst case is linear in nature, O(set size)
+	*/
+
+	un_st.insert(2); 
+	un_st.insert(3);
+	un_st.insert(1); 
+
+	
+	multiset<int> ms; 
+
+	ms.insert(1); 
+	ms.insert(1); 
+	ms.insert(2); 
+	ms.insert(2); 
+	ms.insert(3); // or ms.emplace(3) 
+	cout << "multiset is "; 
+	for(auto it: ms){
+	cout << it << " ";
+
+	}
+
+	ms.erase(2); // all the instances for 2 will be erased 
+	cout << "multiset is after deleting 2 is "; 
+	for(auto it: ms){
+	cout << it << " ";
+
+	}
+
+	// auto it = ms.find(2); // returns an iterator pointing to the first element of 2 
+	 
+	 ms.clear(); // deleted the entire set 
+	/*
+	or 
+	ms.erase(ms.begin(), ms.end()); // deletes the entire set 
+	*/
+	
+	cout << "multiset is after clear method is "; 
+	for(auto it: ms){
+	cout << it << " ";
+
+	}
 
 	return 0;
 }
